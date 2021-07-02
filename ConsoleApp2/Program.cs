ad548860279db55db1f1987ec3f5566afe4bd21f
OSCAR ALEXANDER CASTRO SALAS <oscar.castro21@inacapmail.cl>
2021-07-02 16:58:52 -0400

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            string respuesta;
+            string caracter;
+            string reverso ="";
+            int x;
+            Console.WriteLine("Escribe una palabra y yo te diré si es palíndromo ");
+            respuesta = Convert.ToString(Console.ReadLine());
+            x = respuesta.Length;
+            for (int i = x-1; i >= 0; i--)
+            {
+                caracter = respuesta.Substring(i, 1);
+                reverso = reverso + caracter;
+
+            }   
+            if (reverso == respuesta)
+
+                {
+                    Console.WriteLine("La palabra " + respuesta + " es palíndromo ");
+                }
+            else
+            {
+                Console.WriteLine("La palabra " + respuesta + " NO es palíndromo ");
+            }
+                
+        }
+    }
+}
